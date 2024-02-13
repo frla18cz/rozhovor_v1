2e1b54b590300b6e7d6a0a2d05148f6cfd467e39
frla18cz <70630255+frla18cz@users.noreply.github.com>
2024-02-13 10:10:45 +0100

    feat-new-page 1.02

--- pages/database.py
+++ pages/database.py
@@ -1,6 +1,28 @@
 import streamlit as st
+import mysql.connector
+from modules.database.local_database_login import local_db_login
 
 def database_page_show():
     st.title("Databáze")
     st.write("Testování funkčnosti databáze.")
     # Další kód specifický pro stránku "Databáze"
+
+    try:
+        cnx = mysql.connector.connect(user=st.secrets["database"]["user"],
+                                      password=st.secrets["password"],
+                                      host=st.secrets["database"]["host"],
+                                      port=st.secrets["database"]["port"],
+                                      database=st.secrets["database"]["database"])
+        mycursor = cnx.cursor()
+        query = "SELECT * FROM table_rozhovor ORDER BY id DESC LIMIT 1"
+        mycursor.execute(query)
+
+        myresult = mycursor.fetchall()
+        st.write(f"Připojili jsme se k databázi přes cloud streamlit: {st.secrets["database"]["database"]}\ntestovací výpis:{myresult}")
+        mycursor = cnx.cursor()
+
+
+
+    except mysql.connector.Error as err:
+        st.write("Something went wrong: {}".format(err))
+    return cnx, mycursor
