6eedcaac7fbfc3162b913302cf6b05b84f031c1c
frla18cz <70630255+frla18cz@users.noreply.github.com>
2024-02-14 00:46:15 +0100

    development_v1 update 1.15

--- streamlit_app.py
+++ streamlit_app.py
@@ -73,7 +73,7 @@ with st.sidebar:
     st.subheader("Vytvořit nového asistenta")
     new_assistant_name = st.text_input("Název nového asistenta")
     new_assistant_instructions = st.text_area("Instrukce pro asistenta")
-    new_assistant_model = st.sidebar.selectbox('Vyberte model:',('gpt-4-0125-preview', 'gpt-4-preview', 'gpt-3.5-turbo-16k', 'gpt-3.5-turbo-0125'),index=0, key=0)
+    new_assistant_model = st.sidebar.selectbox('Vyberte model:',('gpt-4-0125-preview', 'gpt-4-preview', 'gpt-3.5-turbo-16k', 'gpt-3.5-turbo-0125'),index=0,  key="new_model_select_key")
     if st.button("Vytvořit"):
         # Zde doplňte logiku pro vytvoření asistenta pomocí OpenAI API
         response = client.beta.assistants.create(name=new_assistant_name, instructions=new_assistant_instructions, model=new_assistant_instructions)
@@ -86,7 +86,7 @@ with st.sidebar:
     st.subheader("Aktualizovat asistenta")
     assistant_to_update = st.selectbox("Vyberte asistenta pro aktualizaci", options=[a[1] for a in asistenti_tuple], index=0)
     updated_instructions = st.text_area("Nové instrukce pro asistenta")
-    model_to_update = st.sidebar.selectbox('Vyberte model:',('gpt-4-0125-preview', 'gpt-4-preview', 'gpt-3.5-turbo-16k', 'gpt-3.5-turbo-0125'),index=0, key=1)
+    model_to_update = st.sidebar.selectbox('Vyberte model:',('gpt-4-0125-preview', 'gpt-4-preview', 'gpt-3.5-turbo-16k', 'gpt-3.5-turbo-0125'),index=0, key="update_model_select_key" )
 
     if st.button("Aktualizovat"):
         # Logikapro aktualizaci asistenta pomocí OpenAI API
