2dacf054578cedf8728e00458e0e73cd2c9805dc
frla18cz <70630255+frla18cz@users.noreply.github.com>
2024-02-13 10:23:16 +0100

    feat-new-page 1.05

--- pages/database.py
+++ pages/database.py
@@ -1,29 +1,38 @@
 import streamlit as st
 import mysql.connector
-from modules.database.local_database_login import local_db_login
+
+
+# Předpokládám, že `local_db_login` je funkce nebo proměnná, kterou zde nepoužíváte, takže ji z příkladu vynechávám.
 
 def database_page_show():
     st.title("Databáze")
     st.write("Testování funkčnosti databáze.")
-    # Další kód specifický pro stránku "Databáze"
 
-    try:
-        cnx = mysql.connector.connect(user=st.secrets["database"]["user"],
-                                      password=st.secrets["database"]["password"],
-                                      host=st.secrets["database"]["host"],
-                                      port=st.secrets["database"]["port"],
-                                      database=st.secrets["database"]["database"])
-        mycursor = cnx.cursor()
-        query = "SELECT * FROM table_rozhovor ORDER BY id DESC LIMIT 1"
-        mycursor.execute(query)
+    # Tlačítko pro spuštění databázového dotazu
+    if st.button('Zobrazit poslední záznam'):
+        try:
+            cnx = mysql.connector.connect(
+                user=st.secrets["database"]["user"],
+                password=st.secrets["database"]["password"],
+                host=st.secrets["database"]["host"],
+                port=st.secrets["database"]["port"],
+                database=st.secrets["database"]["database"]
+            )
+            mycursor = cnx.cursor()
+            query = "SELECT * FROM table_rozhovor ORDER BY id DESC LIMIT 1"
+            mycursor.execute(query)
 
-        myresult = mycursor.fetchall()
-        st.write(f"Připojili jsme se k databázi přes cloud streamlit")
-        st.write(st.secrets["database"]["database"])
-        st.write(f"testovací výpis:{myresult}")
-        mycursor = cnx.cursor()
+            myresult = mycursor.fetchall()
+            if myresult:
+                st.write("Připojili jsme se k databázi přes cloud streamlit.")
+                st.write("Název databáze:", st.secrets["database"]["database"])
+                st.write("Testovací výpis:", myresult)
+            else:
+                st.write("Žádné záznamy nebyly nalezeny.")
 
+            mycursor.close()
+            cnx.close()
 
+        except mysql.connector.Error as err:
+            st.write("Something went wrong: {}".format(err))
 
-    except mysql.connector.Error as err:
-        st.write("Something went wrong: {}".format(err))
