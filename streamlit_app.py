700320abb414bf82c34cf46b6829f7cd78d76210
frla18cz <70630255+frla18cz@users.noreply.github.com>
2024-02-14 01:23:07 +0100

    development_v1 update 1.16

--- streamlit_app.py
+++ streamlit_app.py
@@ -86,7 +86,7 @@ with st.sidebar:
     st.subheader("Aktualizovat asistenta")
     assistant_to_update = st.selectbox("Vyberte asistenta pro aktualizaci", options=[a[1] for a in asistenti_tuple], index=0)
     updated_instructions = st.text_area("Nové instrukce pro asistenta")
-    model_to_update = st.sidebar.selectbox('Vyberte model:',('gpt-4-0125-preview', 'gpt-4-preview', 'gpt-3.5-turbo-16k', 'gpt-3.5-turbo-0125'),index=0, key="update_model_select_key" )
+    model_to_update = st.sidebar.selectbox('Vyberte model:',('gpt-4-0125-preview', 'gpt-4-preview', 'gpt-3.5-turbo-16k', 'gpt-3.5-turbo-0125', 'gpt-4-0613'),index=0, key="update_model_select_key" )
 
     if st.button("Aktualizovat"):
         # Logikapro aktualizaci asistenta pomocí OpenAI API
@@ -94,7 +94,7 @@ with st.sidebar:
         response = client.beta.assistants.update(assistant_id=assistant_id_to_update, instructions=updated_instructions, model=model_to_update)
         if response:
             st.success(f"Asistent {assistant_to_update} byl úspěšně aktualizován.")
-            st.rerun()
+            st.experimental_rerun()
 
     # Sekce pro odstranění asistenta
     st.markdown("---")
